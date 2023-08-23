89392eb0f20c7b6518d726b2a871469a26ecf8ca
nunescode <nunes.adsi@gmail.com>
2023-08-23 09:59:53 -0300

    Atividade 01 - (23.08.23)

---
+++ components/MeuComponente.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function MeuComponente() {
+    return (
+        <View>
+            <Text>
+                Opa, tudo bom!?
+            </Text>
+        </View>
+    );
+}
---
+++ components/MinMax.js
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function MinMax(props) {
+  
+const { min, max } = props;
+const maxValue = Math.max(parseInt(min), parseInt(max));
+const minValue = Math.min(parseInt(max), parseInt(min));
+  
+  return (
+    <View>
+        <Text>O valor máximo é: {maxValue}</Text>
+        <Text>O valor mínimo é: {minValue}</Text>
+    </View>
+  );
+}
